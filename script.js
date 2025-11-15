e90b5cc4a4fddca8a1d94ba299738fa511d00ec1
dhammshila <dhammshila2255@gmail.com>
2025-11-15 19:31:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//function greet() {
+           // alert('Hello from External File');
+//}
+//conlsole.log("some message");
+
+//let varA = "Hello";
+//let varB = 10;
+//let varC = true;
+//const CONSTVAL ={
+     helo:"GREET"
+//};
+//CONSTVAL.helo="so";
+//CONSTVAL["sd"]="sd";
+//console.log(CONSTVAL);
+
+//let str1 = "Hello";
+//let str2 = 'World';
+//let str3 = `Template literal with ${str1}`;
+//console.log(str3);
+function colorChange(){
+    let element = document.getElementById("btn");
+    element.style.backgroundColor = "red"
+}
